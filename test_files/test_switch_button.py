cb1a83085b879b25131a0df388350f00a1b54b15
AndreasNilsson123 <83422758+AndreasNilsson123@users.noreply.github.com>
2023-11-20 11:39:57 +0100

    No change

--- test_files/test_switch_button.py
+++ test_files/test_switch_button.py
@@ -4,14 +4,14 @@ GPIO.setmode(GPIO.BCM)
 from time import sleep
 
 switch_pin = 5
-switch2_pin = 6
-switch3_pin = 19
-switchButton1 = SwitchButton(switch_pin)
-switchButton2 = SwitchButton(switch2_pin)
-switchButton3 = SwitchButton(switch3_pin)
+#switch_pin = 26
+#switch_pin = 19
+switchButton = SwitchButton(switch_pin)
+#switchButton2 = SwitchButton(switch2_pin)
+#switchButton3 = SwitchButton(switch3_pin)
 
 while True:
-    print(switchButton1.is_pressed())
-    print(switchButton2.is_pressed())
-    print(switchButton3.is_pressed())
+    print(switchButton.is_pressed())
+#    print(switchButton.is_pressed())
+#    print(switchButton.is_pressed())
     sleep(0.1)
